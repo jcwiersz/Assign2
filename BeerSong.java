b92a45cd07c1652c768e0627124a1ae2b36c8cbb
Jacob Wierszewski <jcwiersz@oakland.edu>
2016-01-31 16:09:44 -0500

    Altered BeerSong.java for proper coding etiquette indenting

--- BeerSong.java
+++ BeerSong.java
@@ -1,32 +1,26 @@
-c static void main(String[] args) {
-
+public static void main(String[] args) {
 
 
         int bottlesOfBeer = 99; //Initial Number of Bottles to begin with
-	        String word = "bottles";
+        String word = "bottles";
 
-		        while (bottlesOfBeer> 0)
-			        {
-				            if (bottlesOfBeer == 1) {
-					                    word = "bottle";
-							                }
-									            else {
-										                    word = "bottles";
-												                }
-
-														            System.out.println(bottlesOfBeer + " " + word + " of beer on the wall, " + bottlesOfBeer + " " + word + " of beer");
-															                System.out.println("Take one down, pass it around, ");
-
-																	            bottlesOfBeer = bottlesOfBeer - 1;
-
-																		                if (bottlesOfBeer > 0) {
-																				                System.out.println(bottlesOfBeer + " " + word + " of beer on the wall.\n");
-																						            } else {
-																							                    System.out.println("No more bottles of beer on the wall\n");
-																									                }
-
-
-																											        } //End of Song loop
-
-																												    } // End of Ninety_Nine_Bottles_of_Beer
+		while (bottlesOfBeer> 0)
+		 {
+		   if (bottlesOfBeer == 1) {
+		      word = "bottle";
+		    }
+		   else {
+		      word = "bottles";
+		    }
+            System.out.println(bottlesOfBeer + " " + word + " of beer on the wall, " + bottlesOfBeer + " " + word + " of beer");
+            System.out.println("Take one down, pass it around, ");
+            bottlesOfBeer = bottlesOfBeer - 1;
+	
+	           if (bottlesOfBeer > 0) {
+		      System.out.println(bottlesOfBeer + " " + word + " of beer on the wall.\n");
+		   } else {
+		      System.out.println("No more bottles of beer on the wall\n");
+		   }
+																			        } //End of Song loop
+																		    } // End of Ninety_Nine_Bottles_of_Beer
 
